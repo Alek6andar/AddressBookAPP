9c3ebd92bed340719c68455d0b0459c5e6c48d25
Alek6andar <alek6andar@gmail.com>
2023-07-12 21:03:18 +0200

    creating a API with .NET CORE

---
+++ AddressBookApi/AddressBookContext.cs
@@ -0,0 +1,15 @@
+﻿using AddressBookApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace AddressBookApi
+{
+    public class AddressBookContext : DbContext
+    {
+        public AddressBookContext(DbContextOptions<AddressBookContext> options)
+        : base(options)
+        {
+        }
+        public DbSet<Contact> Contacts { get; set; } = null!;
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# AddressBookApp
+
+- Have database instance running, this app requires Postrgres db https://www.postgresql.org/
+- Run Update-Database in package manager console
